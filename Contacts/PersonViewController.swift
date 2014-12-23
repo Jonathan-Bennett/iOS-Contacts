38df986e36d6b02a92159f83eaf599c11d82e95e
Jonathan Bennett <jonathan@jbennett.me>
2014-12-23 01:12:51 -0500

    editing viewed person

--- Contacts/PersonViewController.swift
+++ Contacts/PersonViewController.swift
@@ -27,6 +27,30 @@ class PersonViewController: UIViewController {
     textLabel.text = "Hello there \(name)"
   }
   
+  @IBAction func didTapEdit() {
+    let editViewController = PersonEditViewController.fromStoryboard()
+    let navViewController = UINavigationController(rootViewController: editViewController)
+    
+    
+    editViewController.doneCallback = {
+      if editViewController.name != "" {
+        self.name = editViewController.name
+        self.dismissViewControllerAnimated(true, completion: nil)
+      } else {
+        // TODO: display error message
+      }
+    }
+    
+    editViewController.cancelCallback = {
+      self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    parentViewController?.presentViewController(navViewController, animated: true) {
+      editViewController.name = self.name // TODO: flashes, ewww
+    }
+  }
+
+  
   class func fromStoryboard() -> PersonViewController {
     return UIStoryboard(name: "Person", bundle: nil).instantiateViewControllerWithIdentifier("Person") as PersonViewController
   }
